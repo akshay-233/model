9b056435800ce1e3ac65fb94239bbca97c39ea81
Akshay <akshaychandu233@gmail.com>
2025-09-28 09:58:46 +0530

    Intial commit

---
+++ model.py
@@ -0,0 +1,7 @@
+from sentence_transformers import SentenceTransformer
+
+# Download and save locally
+model = SentenceTransformer("all-MiniLM-L6-v2")
+model.save("./local_model")
+
+print("✅ Model downloaded and saved to ./local_model")
